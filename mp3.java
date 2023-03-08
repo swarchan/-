49c709fa5d18b299bc76384ea33d926927e52b65
chensh14 <sihua1.chen@midea.com>
2023-03-08 17:51:18 +0800

    测试通过

---
+++ mp3.java
@@ -0,0 +1,258 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.Collator;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class mp3 {
+
+    static final String dealSuffix[] = "mp3,wma".split(",");
+    static Set<String> singerList = new HashSet<>();
+    static Set<String> dirMap = new HashSet<>();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("请输入操作内容：\n" +
+                "\t\"1\"：格式化歌手\n" +
+                "\t其他：格式化歌名");
+
+        BufferedReader buf=new BufferedReader(new InputStreamReader(System.in));
+        String op = buf.readLine();
+
+        if("1".equals(op)){
+            beautySinger();
+            System.out.println();
+            System.out.println("处理完成，生成文件：歌手集合.txt");
+        }else{
+            dealSong();
+            System.out.println("处理完成，已替换歌名");
+        }
+    }
+
+    public static void beautySinger() throws Exception{
+        String tmpSinger = "tmp-歌手集合.txt";
+
+        File file = new File(tmpSinger);
+        if(!file.exists()){
+            System.out.println("请填写文件：tmp-歌手集合.txt（使用逗号，句号，或换行分隔）");
+            System.out.println("程序已退出");
+            System.exit(0);
+        }
+
+        //new FileReader(tmpSinger)=默认GBK
+        Set<String> singer = new HashSet<>();
+        try (Scanner sc = new Scanner(new InputStreamReader(new FileInputStream(tmpSinger),StandardCharsets.UTF_8))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if("".equals(line)) continue;
+                //增加每一行
+                singer.add(line);
+                //数据切分
+                singer = splitSet(singer, ",");
+                singer = splitSet(singer, "，");
+                singer = splitSet(singer, "、");
+                singer = splitSet(singer, "。");
+                singer = splitSet(singer, "\\.");
+            }
+        }
+
+        //set做不了排序（排完后，它默认会打乱），只能用数组排序
+        String[] singerList = singer.toArray(new String[singer.size()]);
+        //按拼音排序
+        Arrays.sort(singerList, Collator.getInstance(Locale.CHINA));
+
+        //保存文件
+        String targetSinger = "歌手集合.txt";
+        writeToFileAppend(targetSinger, singerList);
+
+    }
+
+    /**
+     * 写入文件（替换文件）
+     */
+    public static void writeToFile(String fileName,String[] text) throws Exception {
+        File file = new File(fileName);
+        FileWriter fr = null;
+        try {
+            fr = new FileWriter(file);
+            for (String s : text) {
+                fr.write(s);
+                fr.write("\r\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 写入文件（追加）
+     */
+    public static void writeToFileAppend(String fileName, String[] text) {
+        OutputStreamWriter fw2 = null;
+        try {
+            //如果文件存在，则追加内容；如果文件不存在，则创建文件
+            File f = new File(fileName);
+            //new FileWriter(f, true)=默认GBK
+            fw2 = new OutputStreamWriter(new FileOutputStream(f), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw2);
+        for (String s : text) {
+            pw.println(s);
+        }
+        pw.flush();
+        try {
+            fw2.flush();
+            pw.close();
+            fw2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Set<String> splitSet(Set<String> data, String separator){
+        Set<String> result = new HashSet<>();
+        data.forEach(t->{
+            if("".equals(t)) return;
+            //Collections.addAll(result, t.split(separator)));
+            result.addAll(Arrays.stream(t.split(separator)).map(m->m.trim().toUpperCase()).collect(Collectors.toSet()));
+        });
+        return result;
+    }
+
+    /*************************************************************************
+     * * * * * * * * * * * 以下是处理文件 * * * * * * * * * * * *
+     *************************************************************************/
+
+    public static void dealSong() throws Exception {
+        String singerFile = "歌手集合.txt";
+        String dirFile = "处理文件夹.txt";
+
+
+        File file = new File(singerFile);
+        if(!file.exists()){
+            System.out.println("请填写文件：歌手集合.txt(使用英文逗号或换行分隔)");
+            System.out.println("程序已退出");
+            System.exit(0);
+        }
+
+
+        file = new File(dirFile);
+        if(!file.exists()){
+            System.out.println("请填写文件：处理文件夹.txt(使用英文逗号或换行分隔)");
+            System.out.println("程序已退出");
+            System.exit(0);
+        }
+
+        try (Scanner sc = new Scanner(new InputStreamReader(new FileInputStream(singerFile),StandardCharsets.UTF_8))) {
+            while (sc.hasNextLine()) {
+                //处理输入了逗号
+                String line = sc.nextLine().replace(",","").replace("，","");
+                singerList.add(line);
+            }
+        }
+
+        try (Scanner sc = new Scanner(new InputStreamReader(new FileInputStream(dirFile),StandardCharsets.UTF_8))) {
+            while (sc.hasNextLine()) {  //按行读取字符串
+                String line = sc.nextLine().replace(",","").replace("，","").trim();
+                System.out.println(line);
+                //盘符前，隐藏了占位符，:前一位为盘符
+                line = line.substring(line.indexOf(":")-1);
+                dirMap = getFolderList(line);
+            }
+        }
+
+        for (String dir : dirMap) {
+            reNameMp3(dir);
+        }
+
+    }
+
+    /**
+     * 获取根目录下的所有文件夹路径
+     */
+    public static Set<String> getFolderList(String root){
+        File file = new File(root);
+        if (!file.exists()) {
+            System.out.println(root);
+            System.out.println("读取文件夹有误，中文问题？");
+            return null;
+        }
+        Set<String> allFolder=new HashSet<>();
+        findFolder(file,allFolder);
+        return allFolder;
+    }
+
+    /**
+     * 递归获取
+     */
+    private static void findFolder(File file,Set<String> allFolder){
+        //剔除编译路径，文件目录
+        if(file.getAbsolutePath().indexOf("out") > -1) return;
+
+        if(file.isDirectory()){
+            allFolder.add(file.getAbsolutePath());
+            File[] files= file.listFiles();
+            for(File f:files){
+                findFolder(f,allFolder);
+            }
+        }
+    }
+
+    //getAbsolutePath   文件全路径
+    //getParent   不包括文件名
+    //getName   文件名+后缀
+    public static void reNameMp3(String dir){
+        System.out.println("正在处理文件夹：" + dir);
+
+        File file = new File(dir);
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            for(int i=0; i<files.length; i++){
+                //只处理文件
+                if(files[i].isDirectory()) continue;
+
+                String oldName = files[i].getName();
+                String suffix = oldName.substring(oldName.lastIndexOf("."));
+
+                //只处理特定格式
+                if(Arrays.stream(dealSuffix).allMatch(s->suffix.toLowerCase().indexOf(s.toLowerCase())==-1)) continue;
+
+                //歌手
+                String matchSinger = singerList.stream()
+                        .filter(n->oldName.toLowerCase().indexOf(n.toLowerCase())>-1).findFirst().orElse("");
+                if(matchSinger != ""){
+                    matchSinger = matchSinger.toUpperCase(Locale.ROOT);
+                }
+                //歌名
+                String songName = oldName.replace(matchSinger,"")
+                        .replace("-","").replace(suffix,"").trim()
+                        //替换123.
+                        .replaceAll("[\\d]+\\.","");
+
+                //歌手名 + "-" + 歌名 + 后缀
+                String newName;
+                if("".equals(matchSinger)){
+                    newName = songName + suffix;
+                }else{
+                    newName = matchSinger + "-" + songName + suffix;
+                }
+
+                System.out.println("修改前：" + oldName);
+                String newNamePath = files[i].getParent()
+                        + "\\" + newName;
+                System.out.println("修改后：" + newNamePath);
+				files[i].renameTo(new File(newNamePath));
+            }
+        }
+        System.out.println();
+    }
+
+}
